3cbc45dcd6cc925ae92da0c8db8e56e1fc148d3d
Christian Blötner <c.bloetner@gmail.com>
2024-07-01 07:00:01 +0000

    version 0.1.1

--- R/compare.res.R
+++ R/compare.res.R
@@ -10,22 +10,31 @@
 #' parameters
 #' @export
 
-compare.res <- function(object_with,
-                        object_without){
+compare.res <- function(object_with, object_without){
 
   # Extraction of loadings and p-values of models
 
   out.with <- standardizedsolution(object_with)
   out.wo <- standardizedsolution(object_without)
 
-  # Extraction of the lines of interest
+  if(any(colnames(out.with) == "label") | any(colnames(out.wo) == "label")){
+    # Extraction of the lines of interest
 
-  rows.with <- paste(out.with$lhs,
-                     out.with$op,
-                     out.with$rhs)
-  rows.wo <- paste(out.wo$lhs,
-                   out.wo$op,
-                   out.wo$rhs)
+    rows.with <- paste(out.with$lhs,
+                       out.with$op,
+                       out.with$rhs,
+                       out.with$label)
+    rows.wo <- paste(out.wo$lhs,
+                     out.wo$op,
+                     out.wo$rhs,
+                     out.wo$label)}
+  if(!any(colnames(out.with) == "label") | !any(colnames(out.wo) == "label")){
+    rows.with <- paste(out.with$lhs,
+                       out.with$op,
+                       out.with$rhs)
+    rows.wo <- paste(out.wo$lhs,
+                     out.wo$op,
+                     out.wo$rhs)}
 
   # Reduction of both tables to parameters both have in common
 
@@ -36,12 +45,29 @@ compare.res <- function(object_with,
 
   # Produce the heads-on comparison
 
-  compare.res <- data.frame(out.w.red[1:nrow(out.w.red),
-                                      c(1:4, 7)],
-                            out.wo.red[1:nrow(out.wo.red),
-                                       c(4, 7)])
-  colnames(compare.res) <- c("lhs", "op", "rhs", "est.std.with", "p.with",
-                             "est.std.wo", "p.wo")
+  if(any(colnames(out.with) == "label") | any(colnames(out.wo) == "label")){
+
+    compare.res <- data.frame(out.w.red[1:nrow(out.w.red),
+                                        c("lhs", "op", "rhs", "label",
+                                          "est.std", "pvalue")],
+                              out.wo.red[1:nrow(out.wo.red),
+                                         c("est.std", "pvalue")])
+    colnames(compare.res) <- c("lhs", "op", "rhs", "label",
+                               "est.std.with", "p.with",
+                               "est.std.wo", "p.wo")
+  }
+
+  if(!any(colnames(out.with) == "label") | !any(colnames(out.wo) == "label")){
+    compare.res <- data.frame(out.w.red[1:nrow(out.w.red),
+                                        c("lhs", "op", "rhs",
+                                          "est.std", "pvalue")],
+                              out.wo.red[1:nrow(out.wo.red),
+                                         c("est.std", "pvalue")])
+    colnames(compare.res) <- c("lhs", "op", "rhs",
+                               "est.std.with", "p.with",
+                               "est.std.wo", "p.wo")
+
+  }
 
   # Quantify the agreement of the parameter profiles between the two models
 
@@ -53,4 +79,5 @@ compare.res <- function(object_with,
 
   return(list(Results = compare.res,
               r = r))
+
 }
